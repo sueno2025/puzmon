74cce96c994c17923f9df059a56389879db866ea
s_ueno <pandapark09@yahoo.co.jp>
2025-03-27 11:57:38 +0900

    second_commit
    do_battle()の実装

--- puzmon.py
+++ puzmon.py
@@ -2,16 +2,39 @@
 
 #グローバル変数の宣言
 
+monster_names=[
+   'スライム',
+   'ゴブリン',
+   'オオコウモリ',
+   'ウェアウルフ',
+   'ドラゴン',
+   ]
 #関数宣言
 def main():
-    player_name = input('プレーヤー名を入力して下さい>>')
+    while(True):
+        player_name = input('プレーヤー名を入力して下さい>>')
+        if len(player_name) > 0:
+            break
+        else:
+            print('エラー プレイヤー名を入力してください')
     print('*** Puzzle & Monsters ***')
     kills = go_dungeon(player_name)
-    print('*** GAME CLEARED ***')
-    print(f'倒したモンスター数={kills}')
-
+    if kills == len(monster_names):
+        print('*** GAME CLEARED! ***')
+        print(f'倒したモンスター数={kills}')
+    else:
+        print('*** GAME OVER! ***')
 def go_dungeon(player_name):
+    kills = 0
+
     print(f'{player_name}はダンジョンに到着した')
+    for monster_name in monster_names:
+        kills += do_battle(monster_name)
     print(f'{player_name}はダンジョンに制覇した')
-    return 5 
+    return kills
+
+def do_battle(monster_name):
+    print(f'{monster_name}が現れた!')
+    print(f'{monster_name}を倒した!')
+    return 1
 main()
